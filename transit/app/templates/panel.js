664f2e6aa947ed3dc23accbd887bd051b9fa3efe
sneezyG <ahmadgbolly97@gmail.com>
2022-11-06 23:31:16 +0100

    pushing it

--- transit/app/templates/panel.js
+++ transit/app/templates/panel.js
@@ -84,8 +84,11 @@ const section = document.querySelector('#msg > section');
      header.style.visibility = "visible";
      input.style.visibility = "visible";
      section.style.visibility = "visible";
-     
+     sectionWidth = section.offsetWidth;
      window.addEventListener('resize', reset);
+     
+     render();
+     displace(sectionWidth);
      reset();
      
      tag.innerHTML = name + "gddhjdiekdjdnndncbcbfnbdjdjdjdjdjjdjdjddj";
@@ -127,7 +130,21 @@ const section = document.querySelector('#msg > section');
    let inputHeight = input.offsetHeight;
    let headerHeight = header.offsetHeight;
    let availHeight = windowHeight - inputHeight - headerHeight;
-   section.style.height = availHeight + 'px';
+   let sectionHeight = section.offsetHeight;
+   
+   if (sectionHeight >= availHeight) {
+     section.style.height = availHeight + 'px';
+   }else {
+     section.style.height = "none";
+     section.offsetWidth;
+     section.style.height = null;
+   }
+   
+   let newWidth = section.offsetWidth;
+   if (newWidth != sectionWidth) {
+     displace(newWidth);
+     sectionWidth = newWidth;
+   }
      /*
      let sectionHeight = section.style.height;
      console.log(msgHeight, inputHeight, headerHeight, availHeight, sectionHeight);
@@ -184,5 +201,83 @@ const section = document.querySelector('#msg > section');
      elem.offsetWidth;
      elem.style.animation = null;
  }
+ 
+ 
+ 
+ 
+ function render() {
+   for (let key in messages) {
+     let p = document.createElement('p');
+     p.id = "msgDate";
+     let p_span = document.createElement('span');
+     p_span.innerHTML = key;
+     p.append(p_span);
+     section.prepend(p);
+     
+     for (let index in messages[key]) {
+       let item = messages[key][index];
+       let div = document.createElement('div');
+       let outerSpan = document.createElement('span');
+       div.id = item.sender;
+       outerSpan.className = "text";
+       
+       let innerSpan = document.createElement('span');
+       innerSpan.innerHTML = item.msg;
+       
+       let sub = document.createElement('sub');
+       sub.innerHTML = item.time;
+       
+       if (item.status) {
+         let img  = document.createElement('img');
+         img.src = item.status + ".png";
+         img.alt = "status";
+         sub.append(img);
+       }
+       
+       outerSpan.append(innerSpan);
+       outerSpan.append(sub);
+       div.append(outerSpan);
+       section.prepend(div);
+       
+     }
+   }
+ }
+ 
+ 
+ 
+ function displace(width) {
+   let spans = document.querySelectorAll(".text > span");
+   let maxWidth = width * 0.6;
+   for (let elem of spans) {
+     if (elem.offsetWidth > maxWidth) {
+       elem.style.width = maxWidth + "px";
+     }
+     let parent = elem.parentElement;
+     let ancestor = parent.parentElement;
+     if (ancestor.id == "me") {
+        let displace = (width - parent.offsetWidth) - 1;
+        parent.style.left = displace + "px";
+     }
+   }
+ }
+ 
+ 
+ 
+ 
+let messages = {
+   "30 October 2022": [
+     {sender: "me", msg: "gehehe hensn bdbdb dgdhh dbdb ", time: "7:30am", status: "read"}, 
+     {sender: "you", msg: "gehehe", time: "7:45am"},
+     {sender: "you", msg: " gehehe hensn bdbdb", time: "12:50pm"},
+     {sender: "me", msg: "gehehe", time: "1:40pm", status: "read" }
+   ],
+   
+   "06 November 2022": [
+     {sender: "you", msg: "thehjrbdb dndnjdin ebfbcbbcjejne ndyeieokebc bcbbxbxbxbhcj eirbfbdn dnddn", time: "2:00pm"}, 
+     {sender: "me", msg: "thehjr bdbdndnjdinebfb cbbcjejn endyeieoke bcbcbbxbxbxbhcjei rbfbdndnddn", time: "2:20pm", status: "pending"},
+   ]
+};
+
+
  
  
